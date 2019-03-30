af5ca4e107f59318db9d785447b7b0670d53796d
DahamLeee <dlek1011@gmail.com>
2019-03-31 01:50:37 +0900

    first commit

---
+++ Chapter2/SampleScrollView/app/src/main/java/com/example/samplescrollview/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.samplescrollview;
+
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+    ScrollView scrollView;
+    ImageView imageView;
+    BitmapDrawable bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scrollView = findViewById(R.id.scrollView);
+        imageView = findViewById(R.id.imageView); // 레이아웃에 정의된 뷰 객체 참조
+        scrollView.setHorizontalScrollBarEnabled(true); // 수평 스크롤바 사용 기능 설정
+
+        Resources res = getResources();
+        bitmap = (BitmapDrawable)res.getDrawable(R.drawable.image01);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
+    }
+
+    public void onButton1Clicked(View v){
+        changeImage();
+    }
+
+    public void changeImage(){
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.image02);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
+    }
+}
