93ba6324405e742b265cbd3de927017a1eaa1ae7
DahamLeee <dlek1011@gmail.com>
2019-04-03 02:37:38 +0900

    Chapter4

---
+++ Chapter3/Challenge6/app/src/main/java/com/example/challenge6/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.challenge6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar seekbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
