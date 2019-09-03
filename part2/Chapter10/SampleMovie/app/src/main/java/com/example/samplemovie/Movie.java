2fdb3c03d805dde6dc063bae5786e45d00da7bb1
DahamLeee <dlek1011@gmail.com>
2019-09-04 01:06:17 +0900

    Movie API

---
+++ part2/Chapter10/SampleMovie/app/src/main/java/com/example/samplemovie/Movie.java
@@ -0,0 +1,16 @@
+package com.example.samplemovie;
+
+public class Movie {
+    String id;
+    String title;
+    String title_eng;
+    String date;
+    String user_rating;
+    String audience_rating;
+    String reviewer_rating;
+    String reservation_rate;
+    String reservation_grade;
+    String grade;
+    String thumb;
+    String image;
+}
---
+++ part2/Chapter10/SampleMovie/app/src/main/java/com/example/samplemovie/MovieList.java
@@ -0,0 +1,7 @@
+package com.example.samplemovie;
+
+import java.util.ArrayList;
+
+public class MovieList {
+    ArrayList<Movie> result = new ArrayList<Movie>();
+}
